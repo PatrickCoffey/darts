4432771a75387e949d1d15a846ab86718928ee49
schlerp <patrickcoffey91@gmail.com>
2016-11-09 22:48:47 +0930

    added notice model and started view

--- darts/noticeboard/models.py
+++ darts/noticeboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class notice(models.Model):
+    title = models.CharField(max_length=100)
+    post_time = models.DateTimeField(default=timezone.now)
+    author = models.CharField(max_length=100)
+    text = models.CharField()
+
+    
--- darts/noticeboard/views.py
+++ darts/noticeboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime, timedelta
+
+from noticeboard.models import notice
 
 # Create your views here.
+
+def noticeboard(request):
+    last_month = datetime.utcnow() - timedelta(months=1)
+    posts = notice.objects.filter(created_at__gt=last_month)
+    tempate = 
